acc5746378687a026c85736b73d68ed1481a06f0
nnemqqq <denistrotsko3@gmail.com>
2024-10-10 09:33:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,15 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFFFFF8F0),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Row(
-                  children: [
-                  Icon(Icons.call),
-                  Text("+380964943694")
-                  ],  
-                ),
-               Row(
-              children: [
-                  Icon(Icons.alternate_email),
-                  Text("Criper@gmail.com")
-              ]
-            ),
-              ],
-            ),
-            Text("Hi, I'm Criper",style: TextStyle(fontSize: 15, color: Color(0xFF291854))),
-            Text("I play to minecraft",style: TextStyle(fontSize: 48, color: Color(0xFFFFF8F0))),
-          ],
-        ),
-      ),
-    ),
-  }
+void main() {  
+Map<String, dynamic> person = {  
+    'name': 'Denis',  
+    'age': '15',  
+    'hobby': 'Ігри, волейбол'  
+  };  
+  print(person);  
+   
+  var map = {  
+    "key" : ["Ігри", "Волейбол"]  
+  };  
+  print(map["key"]);  
+   
+  
 }
